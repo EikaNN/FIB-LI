cb86dc334d5cc47d59fe1d0f919f96deb4e05d99
EikaNN <octavi94@gmail.com>
2016-05-24 00:04:21 +0200

    Practica6 ENDED!

--- Practica6/tomografia.pl
+++ Practica6/tomografia.pl
@@ -25,7 +25,7 @@ ejemplo2( [10,4,8,5,6], [5,3,4,0,5,0,5,2,2,0,1,5,1] ).
 ejemplo3( [11,5,4], [3,2,3,1,1,1,1,2,3,2,1] ).
 
 
-p:-	ejemplo1(RowSums,ColSums),
+p:-	ejemplo3(RowSums,ColSums),
 	length(RowSums,NumRows),
 	length(ColSums,NumCols),
 	NVars is NumRows*NumCols,
@@ -34,15 +34,15 @@ p:-	ejemplo1(RowSums,ColSums),
 	matrixByRows(L,NumCols,MatrixByRows),
 	transpose(MatrixByRows, MatrixByCols),
 	declareConstraints(MatrixByRows, MatrixByCols, RowSums, ColSums),
-	labeling([ff], MatrixByRows),
+	labeling([ff], L),
 	pretty_print(RowSums,ColSums,MatrixByRows).
 
 
 pretty_print(_,ColSums,_):- write('     '), member(S,ColSums), writef('%2r ',[S]), fail.
 pretty_print(RowSums,_,M):- nl,nth1(N,M,Row), nth1(N,RowSums,S), nl, writef('%3r   ',[S]), member(B,Row), wbit(B), fail.
 pretty_print(_,_,_).
-wbit(1):- write('*  '),!.
-wbit(0):- write('   '),!.
+wbit(1):- write('*  '), !.
+wbit(0):- write('   '), !.
 
 listVars(0, []) :- !.
 listVars(N, [_|L1]) :- N1 is N-1, listVars(N1, L1).
@@ -69,4 +69,4 @@ declareConstraints(MatrixByRows, MatrixByCols, RowSums, ColSums) :-
 %	declareConstraints(MatrixByRows, MatrixByCols, RowSums, ColSums).
 %declareConstraints([], [], [], []).
 
-main :- p.
+main :- p, nl, halt.
